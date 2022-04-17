07bcafe1eba747f8c22ba35a18fa20331301a845
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-04-17 14:06:18 +0700

    update accesstoken

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,23 +1,21 @@
 import axios from "axios";
 import store from "../app/store";
 
-
-
+let accessToken;
 try {
   const root = JSON.parse(localStorage.getItem("persist:root") || "");
-  var user = JSON.parse(root.authReducer);
+  const user = JSON.parse(root.authReducer);
+  if (user) accessToken = user.accessToken;
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
-
-
 
 
 const fetchAPI = axios.create({
   baseURL: "https://nodejs-express-duon97ran2.vercel.app/api/",
   timeout: 30000,
   headers: {
-    "Authorization": `Bearer ${user.accessToken}`,
+    "Authorization": `Bearer ${accessToken}`,
   }
 });
 
